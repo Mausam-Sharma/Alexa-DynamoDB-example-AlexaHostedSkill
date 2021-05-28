5a8d3ed72a57c4374fd4c8ab98b4a22118f7d60f
Mausam-Sharma <35280772+Mausam-Sharma@users.noreply.github.com>
2021-05-28 19:24:27 +0530

    first commit

---
+++ lambda/index.js
@@ -0,0 +1,110 @@
+const Alexa = require('ask-sdk');
+const ddbAdapter = require('ask-sdk-dynamodb-persistence-adapter');
+const AWS = require('aws-sdk');
+
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    async handle(handlerInput) {
+        const speakOutput = 'Dynamo DB persistent storage example, you have launched the skill ';
+        
+        const persistentAttributes = await  handlerInput.attributesManager.getPersistentAttributes();
+        
+        console.log(persistentAttributes);
+        
+        var counter ;
+        if(persistentAttributes && Object.keys(persistentAttributes).length === 0){
+            counter = 0;
+        }
+        else{
+            counter = persistentAttributes.counter;
+        }
+        
+        counter = counter+1;
+        
+        console.log(counter);
+        
+        handlerInput.attributesManager.setPersistentAttributes({
+            counter: counter
+        });
+      
+        await handlerInput.attributesManager.savePersistentAttributes();
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput + counter + ' times')
+            .getResponse()
+    }
+};
+
+
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        )
+    .addErrorHandlers(
+        ErrorHandler)
+    .withPersistenceAdapter(
+        new ddbAdapter.DynamoDbPersistenceAdapter({
+            tableName: process.env.DYNAMODB_PERSISTENCE_TABLE_NAME,
+            createTable: false,
+            dynamoDBClient: new AWS.DynamoDB({apiVersion: 'latest', region: process.env.DYNAMODB_PERSISTENCE_REGION})
+        })
+    )
+    .lambda();
